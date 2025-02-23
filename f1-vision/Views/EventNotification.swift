9ec8158bbf57c60d7b8c0871f008253f2659a176
Mahit Mehta <mahit.py@gmail.com>
2025-02-23 02:44:31 -0500

    scheduler

---
+++ f1-vision/Views/EventNotification.swift
@@ -0,0 +1,18 @@
+//
+//  EventNotification.swift
+//  f1-vision
+//
+//  Created by Mahit Mehta on 2/23/25.
+//
+
+import SwiftUI
+
+struct EventNotification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventNotification()
+}
